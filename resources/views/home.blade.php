948be40b1582448d782b5566fbe31e55421045a3
ahyas <ahyasw@gmail.com>
2023-05-27 16:01:17 +0900

    Change view, make it wide

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,59 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="container-fluid">
+    <div class="card">
+        <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <table class="table table-striped border">
-                        <tr>
-                            <th>ID Perkara</th>
-                            <th>Tanggal pendaftaran</th>
-                            <th>Jenis perkara</th>
-                            <th>Nomor perkara</th>
-                            <th>Tahapan terakhir</th>
-                            <th></th>
-                        </tr>
-                        @foreach($perkara as $row)
-                        <tr>
-                            <td>{{$row->perkara_id}}</td>
-                            <td>{{$row->tanggal_pendaftaran}}</td>
-                            <td>{{$row->jenis_perkara_text}}</td>
-                            <td>{{$row->nomor_perkara}}</td>
-                            <td>{{$row->tahapan_terakhir_text}}</td>
-                            <td></td>
-                        </tr>
-                        
-                            <tr>
-                                <th></th><th>Pihak ID</th><th>Nama</th><th colspan="2">Alamat</th><th>No. Identitas</th>
-                            </tr>
-                            @foreach($para_pihak as $baris)
-                                @if($baris->perkara_id == $row->perkara_id)
-                                    <tr>
-                                        <td></td><td>{{$baris->pihak_id}}</td><td>{{$baris->nama}}</td><td colspan="2">{{$baris->alamat}}</td>
-                                        <td>
-                                        @foreach($pihak_info as $row_pihak_info)
-                                            @if($row_pihak_info->id == $baris->pihak_id)
-                                                {{$row_pihak_info->nomor_indentitas}}
-                                            @endif
-                                        @endforeach
-                                        </td>
-                                    </tr>
-                                @endif
-                            @endforeach
-                        @endforeach
-                    </table>
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+
+            <table class="table table-striped border">
+                <tr>
+                    <th>ID Perkara</th>
+                    <th>Tanggal pendaftaran</th>
+                    <th>Jenis perkara</th>
+                    <th>Nomor perkara</th>
+                    <th>Tahapan terakhir</th>
+                    <th>No. Akta cerai</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                @foreach($perkara as $row)
+                <tr>
+                    <td>{{$row->perkara_id}}</td>
+                    <td>{{$row->tanggal_pendaftaran}}</td>
+                    <td>{{$row->jenis_perkara_text}}</td>
+                    <td>{{$row->nomor_perkara}}</td>
+                    <td>{{$row->tahapan_terakhir_text}}</td>
+                    <td>{{$row->nomor_akta_cerai}}</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                
+                    <tr>
+                        <th></th><th>Pihak ID</th><th>Nama</th><th colspan="2">Alamat</th><th>No. Identitas</th><th>No. Telp.</th><th>Email</th>
+                    </tr>
+                    @foreach($para_pihak as $baris)
+                        @if($baris->perkara_id == $row->perkara_id)
+                            <tr>
+                                <td></td><td>{{$baris->pihak_id}}</td><td>{{$baris->nama}}</td><td colspan="2">{{$baris->alamat}}</td>
+                                @foreach($pihak_info as $row_pihak_info)
+                                    @if($row_pihak_info->id == $baris->pihak_id)
+                                        <td>{{$row_pihak_info->nomor_indentitas}}</td>
+                                        <td>{{$row_pihak_info->telepon}}</td>
+                                        <td>{{$row_pihak_info->email}}</td>
+                                    @endif
+                                @endforeach
+                            </tr>
+                        @endif
+                    @endforeach
+                @endforeach
+            </table>
         </div>
     </div>
 </div>
